9e1b6a36cb95c01fdaf14e8651db8537c8808457
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-08-26 07:20:58 +0200

    added heart ico & js steps

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+console.log('JS OK')
+
+/* 
+1. Recuperare gli elementi d'intresse dal DOM 
+
+2. creare una funzione per generare le celle
+
+3. generare con la funzione 100 celle in righe da 10 
+ ed inserirle al interno del elemento recuperato dal DOM
+
+3. inserire all'interno delle celle il suo numero 
+    corrispondente che vanno i ordine dal 1 al 100
+
+5.  Creare un evento che al click aggiunga in console il numero 
+    della cella cliccata eppure cambi il background della cella. 
+
+*/
