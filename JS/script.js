01bb96e63c532325bcd2d20f77fbe4100c1bf487
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-08-26 08:26:29 +0200

    added cells innertext & create event listener Esercizio completato

--- JS/script.js
+++ JS/script.js
@@ -24,9 +24,8 @@ console.log('JS OK')
 //1. Recuperare gli elementi d'intresse dal DOM 
 
 const grid = document.getElementById('grid');
-const btnPlay = document.getElementById('Play-btn');
+const Submit = document.getElementById('Play-btn');
 
-console.log(grid, btnPlay);
 
 //1b. Creo le variabile per creare il nr di righe e colonne desiderate
 const rows = 10;
@@ -36,9 +35,10 @@ const totCells = rows * col;
 
 //2. creare una funzione per generare le celle
 
-function generateCell() {
+function generateCell(content) {
     const cell = document.createElement('div');
     cell.className = 'cell';
+    cell.append(content)
     return cell;
 }
 
@@ -46,10 +46,23 @@ function generateCell() {
 
 /* 3. Generare il nr di celle desiderate usando un ciclo for e la funzione creata in precedenza
  ed inserirle al interno del elemento recuperato dal DOM */
+Submit.addEventListener('click', function (event) {
+    event.preventDefault
+    for (let i = 1; i <= totCells; i++) {
 
-for (let i = 0; i < totCells; i++) {
-    const cell = generateCell();
+        /* 4. inserire all'interno delle celle il suo numero 
+        corrispondente che vanno i ordine dal 1 al 100 */
+        const cell = generateCell(i);
 
-    grid.appendChild(cell)
-}
+        /*  5.  Creare un evento che al click aggiunga in console il numero 
+        della cella cliccata eppure cambi il background della cella. 
+        */
+        cell.addEventListener('click', function () {
+            cell.classList.add('clicked');
+            console.log(cell.innerText)
+        })
 
+        grid.appendChild(cell)
+    }
+
+})
